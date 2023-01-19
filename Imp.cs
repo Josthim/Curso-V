be965363a34a70254a8100999155fc490816a12f
Josthim <110624763+Josthim@users.noreply.github.com>
2023-01-19 11:25:22 -0600

    Add files via upload

---
+++ Imp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Imp
+    {
+        double[] Ing = new double[10];
+        double[] Il = new double[10];
+        double[] Ip = new double[10];
+        //double Ing[10];
+        //double Il[10];
+        //double Ip[10];
+
+        public Imp() { }
+        public Imp(double[] ing, double[] il, double[] ip)
+        {
+
+            Ing = ing;
+            Il = il;
+            Ip = ip;
+        }
+        public void setIng(double[] ing)
+        {
+            this.Ing = ing;
+        }
+        public int Validar(String Mensaje)
+        {
+            int num = 0, v;
+            do
+            {
+                v = 1;
+                try
+                {
+                    Console.WriteLine(Mensaje);
+                    num = Convert.ToInt32(Console.ReadLine());
+
+                }
+                catch (Exception error)
+                {
+                    v = 0;
+                    Console.WriteLine("NO SE ACEPTAN LETRAS", error.Message);
+                }
+            } while (v == 0);
+            return num;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using ConsoleApp1;
+public class ConsolaImpuestos
+{
+    public static  void Main(String[]args)
+    {
+        Imp im = new Imp();
+        double[] S; double[] P; double[] L;
+        S = new double[10];
+        P = new double[10];
+        L = new double[10];
+        int ind = 0; int ce;
+
+        do
+        {
+
+            ce = im.Validar("Cantidad de elementos");
+
+        }while (ce <= 0 || ce > 10);
+        for (ind = 0; ind < ce; ind++)
+        {
+            do
+            {
+
+
+                S[ind] = im.Validar("Valor;");
+                im.setIng(S);
+            } while (S[ind]<4800 || S[ind]> 500000);
+            P[ind] = S[ind] * 0.07;
+            L[ind] = S[ind] * 0.22;
+        }  
+
+        for (ind = 0; ind < ce; ind++ )
+        {
+            
+            Console.WriteLine("\t" + S[ind]);
+
+        }
+        for (ind = 0; ind < ce; ind++)
+        {
+            Console.WriteLine("\t" + P[ind]);
+
+        }
+        for (ind = 0; ind < ce; ind++)
+        {
+            Console.WriteLine("\t" +L[ind]);
+
+        }
+
+    }
+}
