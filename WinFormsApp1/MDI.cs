ab87b2b179f9ae5c5c42e6ade74e03f518ee6a0e
Josthimx1 <110624763+Josthim@users.noreply.github.com>
2023-01-23 10:52:43 -0600

    Add files via upload

---
+++ WinFormsApp1/MDI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class MDI : Form
+    {
+        Form1 x = new Form1();
+        public MDI()
+        {
+            InitializeComponent();
+        }
+
+        private void innsLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            x.MdiParent = this;
+            x.Show();
+
+        }
+    }
+}
