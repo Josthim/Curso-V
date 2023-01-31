0217339786cc51326f8c42f7e5e9f44a2e1c75ac
Josthimx1 <110624763+Josthim@users.noreply.github.com>
2023-01-31 15:09:57 -0600

    Add files via upload

---
+++ zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz/pjPrueba/Form3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pjPrueba
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox5.Enabled = false;
+        }
+    }
+}
